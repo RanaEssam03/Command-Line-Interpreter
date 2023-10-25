56cc1897740e4691a81e617ac94810dd0f6e63c7
Rana Essam <95047435+RanaEssam03@users.noreply.github.com>
2023-10-25 15:01:08 +0300

    adding classes and pwd command

---
+++ src/Commands.java
@@ -0,0 +1,12 @@
+/// Created at: 25/10/2023
+/// Last modification: 25/10/2023
+
+//This enum contains all the commands that the user can enter in the terminal
+public enum Commands {
+    echo,
+    pwd,
+    cd,
+    ls,
+    mkdir,
+}
+
---
+++ src/Parser.java
@@ -0,0 +1,42 @@
+/// Created at: 25/10/2023
+/// Last modification: 25/10/2023
+
+
+/**
+ * This class will parse the input command and divide it into commandName and args
+ */
+public class Parser {
+   private String commandName;
+   private String[] args;
+
+
+    /**
+     * This method will divide the input into commandName and args
+     * @param input  is the string command entered by the user
+     * @return true if the command is valid and false otherwise
+     */
+    public boolean parse(String input) {
+        String[] inputArray = input.split(" ");
+        commandName = inputArray[0];
+        try {
+            Commands.valueOf(commandName);
+            args = new String[inputArray.length - 1];
+            for (int i = 1; i < inputArray.length; i++) {
+                args[i - 1] = inputArray[i];
+            }
+
+        } catch (IllegalArgumentException ex) {
+            System.out.println("Invalid command!");
+            return false;
+        }
+
+        return true;
+    }
+
+    public String getCommandName(){
+        return commandName;
+    }
+    public String[] getArgs(){
+        return args;
+    }
+}
